07459619858a6d4db5aac870474b669990c60f27
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2022-06-01 22:58:31 +0200

    remove name from extension code

--- src/cssContentScript/index.js
+++ src/cssContentScript/index.js
@@ -1,23 +1,24 @@
 (function() {
-  if (document.querySelector('#plussub-cue-style')) {
+  const EXTENSION_ORIGIN = "uitjeuin";
+  if (document.querySelector(`#${EXTENSION_ORIGIN}-cue-style`)) {
     return;
   }
 
   const style = document.createElement('style');
-  style.id = 'plussub-cue-style';
+  style.id = `${EXTENSION_ORIGIN}-cue-style`;
 
   style.innerHTML = `
-  video[data-plus-sub-status="injected"]::cue{
-    color: var(--plusSub-cue-color);
-    background-color: var(--plusSub-cue-background-color);
-    font-size: calc(var(--plusSub-cue-font-size)*1px);
-    font-weight: var(--plusSub-cue-font-weight);
-    line-height: var(--plusSub-cue-line-height);
+  video[data-${EXTENSION_ORIGIN}-status="injected"]::cue{
+    color: var(--${EXTENSION_ORIGIN}-cue-color);
+    background-color: var(--${EXTENSION_ORIGIN}-cue-background-color);
+    font-size: calc(var(--${EXTENSION_ORIGIN}-cue-font-size)*1px);
+    font-weight: var(--${EXTENSION_ORIGIN}-cue-font-weight);
+    line-height: var(--${EXTENSION_ORIGIN}-cue-line-height);
 
-    background-color: var(--plusSub-cue-background-color);
-    background-image: linear-gradient(var(--plusSub-cue-background-color), var(--plusSub-cue-background-color));
-    border-color: var(--plusSub-cue-background-color);
-    text-decoration-color: var(--plusSub-cue-background-color);
+    background-color: var(--${EXTENSION_ORIGIN}-cue-background-color);
+    background-image: linear-gradient(var(--${EXTENSION_ORIGIN}-cue-background-color), var(--${EXTENSION_ORIGIN}-cue-background-color));
+    border-color: var(--${EXTENSION_ORIGIN}-cue-background-color);
+    text-decoration-color: var(--${EXTENSION_ORIGIN}-cue-background-color);
   }
 `;
     document.head.append(style);
